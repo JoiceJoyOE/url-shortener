7986f67d8c775e5466c072fbdd229ef048992799
Joice Joy <joice.joy.oe@gmail.com>
2020-02-20 11:24:14 +0530

    more

--- server/core/system.js
+++ server/core/system.js
@@ -7,13 +7,16 @@ const express = require('express');
 const session = require('express-session');
 const knexSessionStore = require('connect-session-knex')(session);
 const _ = require('lodash');
-
+const http = require('http');
+const ip = require('ip');
 
 module.exports = {
     async init() {
-        const SYSTEM = {};
+        const SYSTEM = {
+            config: {}
+        };
         SYSTEM.config.cors = {
-            credentials: true,
+            credentials: false,
             maxAge: 600,
             methods: 'GET,POST',
             origin: true
@@ -22,14 +25,24 @@ module.exports = {
         SYSTEM.app = app;
         app.use(compression);
         const securityHeaders = require('../security/security');
-        app.use(securityHeaders);
-        app.use(cors(SYSTEM.config.cors));
-        app.options('*', cors(SYSTEM.config.cors));
+        // app.use(securityHeaders);
+        // app.use(cors(SYSTEM.config.cors));
+        // app.options('*', cors(SYSTEM.config.cors));
 
-        const ssl = require('../security/ssl');
-        app.use('/', ssl);
-        app.use(cookieParser);
+        // const ssl = require('../security/ssl');
+        // app.use('/', ssl);
+        // app.use(cookieParser);
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.get('/hello', (req, res) => {
+            res.status(200).send({
+                message: 'Hello World!'
+            })
+        })
         
-        return {};
+        app.listen(8080, ip.address(), () => {
+            console.log('server running on port: ' + 8080);
+        });
+        return SYSTEM;
     }
 }
